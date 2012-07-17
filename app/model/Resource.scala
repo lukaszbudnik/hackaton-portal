59363d761d602a06d34a35fa281b653503a10ac8
bykes <daniel.bykowski@gmail.com>
2012-07-17 23:42:23 +0200

    simplify resource class / table name

---
+++ app/model/Resource.scala
@@ -0,0 +1,38 @@
+package model
+
+import org.squeryl.PrimitiveTypeMode._
+import org.squeryl.KeyedEntity
+import org.squeryl.Schema
+
+case class Resource(url : String, publicId: String) extends KeyedEntity[Long] {
+  val id: Long = 0L
+}
+
+object Resource extends Schema {
+  protected[model] val resources = table[Resource]("resources")
+  on(resources)(r => declare(r.id is (primaryKey, autoIncremented("resource_id_seq"))))
+
+  def all(): Iterable[Resource] = {
+    resources.toIterable
+  }
+
+  def lookup(id: Long): Option[Resource] = {
+    resources.lookup(id)
+  }
+
+  def insert(r: Resource): Resource = {
+    resources.insert(r)
+  }
+
+  def update(id: Long, res: Resource): Int = {
+    resources.update(r =>
+      where(r.id === id)
+        set (
+          r.url := res.url,
+          r.publicId := res.publicId))
+  }
+
+  def delete(id: Long): Int = {
+    resources.deleteWhere(r => r.id === id)
+  }
+}
