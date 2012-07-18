732e11ef646526e18eeec8a88dd93a6aa47b9ded
bykes <daniel.bykowski@gmail.com>
2012-07-18 16:38:43 +0200

    fix security tests

--- test/helpers/SecuritySpec.scala
+++ test/helpers/SecuritySpec.scala
@@ -14,17 +14,12 @@ class SecuritySpec extends Specification {
       Security.verifyIfAllowed(false)(null) must throwA[SecurityAbuseException]
     }
     
-    "should throw SecurityAbuseException when user is not in given role" in {
+    "should throw SecurityAbuseException when user is not an admin" in {
       implicit val socialUser = SocialUser(UserId("id", "providerId"), "displayName", Some("email"), Some("avatarUrl"), AuthenticationMethod.OpenId, None, None, 123, Seq("viewer"))
       
-      Security.verifyIfAllowed("admin") must throwA[SecurityAbuseException]
+      Security.verifyIfAllowed() must throwA[SecurityAbuseException]
     }
-    
-    "should throw SecurityAbuseException when user is not in all given roles" in {
-      implicit val socialUser = SocialUser(UserId("id", "providerId"), "displayName", Some("email"), Some("avatarUrl"), AuthenticationMethod.OpenId, None, None, 123, Seq("viewer"))
-      
-      Security.verifyIfAllowed("viewer", "editor") must throwA[SecurityAbuseException]
-    }
+
   }
   
 }
