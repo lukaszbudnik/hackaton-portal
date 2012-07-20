907c0d6ec4a9be42f337c317e15ffc3ea0b30b79
bykes <daniel.bykowski@gmail.com>
2012-07-20 12:38:16 +0200

    disable non production code

--- app/Global.scala
+++ app/Global.scala
@@ -1,12 +1,23 @@
-import org.squeryl.adapters.{H2Adapter, PostgreSqlAdapter}
+import org.squeryl.adapters.H2Adapter
+import org.squeryl.adapters.PostgreSqlAdapter
 import org.squeryl.internals.DatabaseAdapter
-import org.squeryl.{Session, SessionFactory}
-import org.squeryl.PrimitiveTypeMode._
-import play.api._
-import play.api.db.DB
-import play.api.mvc._
-import play.api.mvc.Results._
+import org.squeryl.Session
+import org.squeryl.SessionFactory
+
 import core.SecurityAbuseException
+import play.api.db.DB
+import play.api.mvc.Results.Forbidden
+import play.api.mvc.Results.InternalServerError
+import play.api.mvc.Results.NotFound
+import play.api.mvc.Handler
+import play.api.mvc.RequestHeader
+import play.api.mvc.Result
+import play.api.Application
+import play.api.GlobalSettings
+import play.api.Mode
+import play.api.Play
+import play.api.PlayException
+import play.api.UnexpectedException
 
 object Global extends GlobalSettings {
 
@@ -18,28 +29,37 @@ object Global extends GlobalSettings {
     }
   }
 
-  
   override def onError(request: RequestHeader, ex: Throwable): Result = {
     ex.getCause() match {
       case e: SecurityAbuseException => Forbidden(views.html.errors.securityAbuse(e))
       case _ => InternalServerError(Play.maybeApplication.map {
-		      case app if app.mode == Mode.Dev => views.html.defaultpages.devError.f
-		      case app => views.html.defaultpages.error.f
-		      }.getOrElse(views.html.defaultpages.devError.f) {
-		    	  ex match {
-		    	  case e: PlayException.UsefulException => e
-		    	  case e => UnexpectedException(unexpected = Some(e))
-		    	  }
-		      })
+        case app if app.mode == Mode.Dev => views.html.defaultpages.devError.f
+        case app => views.html.defaultpages.error.f
+      }.getOrElse(views.html.defaultpages.devError.f) {
+        ex match {
+          case e: PlayException.UsefulException => e
+          case e => UnexpectedException(unexpected = Some(e))
+        }
+      })
     }
   }
-  
+
   override def onHandlerNotFound(request: RequestHeader): Result = {
-    
-    NotFound(Play.maybeApplication.map {
-      case app if app.mode == Mode.Dev => views.html.defaultpages.devNotFound.f
-      case app => views.html.errors.notFound.f
-    }.getOrElse(views.html.errors.notFound.f)(request, Play.maybeApplication.flatMap(_.routes)))
+    NotFound(
+      Play.maybeApplication.map {
+        case app if app.mode == Mode.Dev => views.html.defaultpages.devNotFound.f
+        case app => views.html.errors.notFound.f
+      }.getOrElse(views.html.errors.notFound.f)(request, Play.maybeApplication.flatMap(_.routes)))
+  }
+
+  override def onRouteRequest(request: RequestHeader): Option[Handler] = {
+    if (play.Play.isProd() &&
+      (request.path.contains("hackathons") ||
+        request.path.contains("sponsors") ||
+        request.path.contains("locations"))) {
+      return None
+    }
+    super.onRouteRequest(request)
   }
 
   private def getSession(adapter: DatabaseAdapter, app: Application) = Session.create(DB.getConnection()(app), adapter)
