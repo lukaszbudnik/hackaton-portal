eb7a953e38f9a5fe24a1237cc52c95cff065cf78
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-07-21 07:25:03 +0200

    removed old imports

--- app/plugin/cloudimage/CloudImage.scala
+++ app/plugin/cloudimage/CloudImage.scala
@@ -1,22 +1,5 @@
 package plugin.cloudimage
 
-import org.apache.commons.codec.digest.DigestUtils
-import org.apache.commons.io.IOUtils
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.mime.content.ByteArrayBody
-import org.apache.http.entity.mime.content.StringBody
-import org.apache.http.entity.mime.HttpMultipartMode
-import org.apache.http.entity.mime.MultipartEntity
-import org.apache.http.impl.client.DefaultHttpClient
-import org.apache.http.HttpResponse
-
-import play.api.i18n.Messages
-import play.api.libs.json.Json
-import play.api.Application
-import play.api.Logger
-import play.api.Play
-import play.api.Plugin
-
 abstract class CloudImageResponse
 
 case class CloudImageErrorResponse(val message: String) extends CloudImageResponse
