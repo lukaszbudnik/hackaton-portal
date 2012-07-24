b96086d04c93766001d173afaa9c57bb16790a61
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-07-24 17:04:18 +0200

    added caching

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,24 +1,33 @@
 package controllers
 
 import play.api.mvc.Controller
+import play.api.cache.Cached
+import play.api.Play.current
+import play.api.mvc.Action
+import play.api.Logger
 
 object Application extends Controller with securesocial.core.SecureSocial {
 
-  def index = UserAwareAction { implicit request =>
-    Ok(views.html.index(request.user))
+  def index = Cached("application.index") {
+    UserAwareAction { implicit request =>
+      Ok(views.html.index(request.user))
+    }
   }
 
-  def about = UserAwareAction { implicit request =>
-    Ok(views.html.about(request.user))
+  def about = Cached("application.about") {
+    UserAwareAction { implicit request =>
+      Ok(views.html.about(request.user))
+    }
   }
 
-  def contact = UserAwareAction { implicit request =>
-    Ok(views.html.contact(request.user))
+  def contact = Cached("application.contact") {
+    UserAwareAction { implicit request =>
+      Ok(views.html.contact(request.user))
+    }
   }
 
   def profile = SecuredAction() { implicit request =>
     Ok(views.html.profile(request.user))
   }
-
 
 }
