ba927dc4bd60370d82e7de36b206018f271df0ea
bykes <daniel.bykowski@gmail.com>
2012-07-25 13:03:03 +0200

    clean sponsors

--- app/controllers/Sponsor.scala
+++ app/controllers/Sponsor.scala
@@ -6,7 +6,7 @@ import org.squeryl.PrimitiveTypeMode.__thisDsl
 import org.squeryl.PrimitiveTypeMode.long2ScalarLong
 import org.squeryl.PrimitiveTypeMode.transaction
 
-import model.Resource
+import play.api.Play.current
 import play.api.data.Forms.boolean
 import play.api.data.Forms.list
 import play.api.data.Forms.longNumber
@@ -23,11 +23,10 @@ import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
 import play.api.mvc.Controller
 import play.api.Logger
-import play.api.Play.current
 import plugins.cloudimage.CloudImageErrorResponse
-import plugins.cloudimage.CloudImageSuccessResponse
-import plugins.cloudimage.CloudImageService
 import plugins.cloudimage.CloudImagePlugin
+import plugins.cloudimage.CloudImageService
+import plugins.cloudimage.CloudImageSuccessResponse
 import plugins.use
 
 object Sponsor extends Controller with securesocial.core.SecureSocial {
@@ -35,193 +34,108 @@ object Sponsor extends Controller with securesocial.core.SecureSocial {
   val sponsorForm = Form(
     mapping(
       "name" -> nonEmptyText,
+      "title" -> nonEmptyText,
       "description" -> nonEmptyText,
       "website" -> nonEmptyText,
       "order" -> number,
-      "isGeneralSponsor" -> boolean,
-      "hackathons" -> list(mapping(
-        "hackathonId" -> longNumber,
-        "order" -> number)((model.HackathonSponsorHelper).apply)(model.HackathonSponsorHelper.unapply)),
+      "hackathonId" -> optional(longNumber),
       "logoResourceId" -> optional(longNumber))(model.Sponsor.apply)(model.Sponsor.unapply))
 
   val uploadError = toJson(Seq(toJson(Map("error" -> toJson(Messages("fileupload.server.error"))))))
 
   def index = UserAwareAction { implicit request =>
     transaction {
-
-      val sponsors: Map[Long, model.Sponsor] = model.Sponsor.all.map({ s => (s.id, s) }).toMap
-
-      val generalSponsors: Iterable[Long] = model.Sponsor.allGeneralSponsorsOrdered.map { s => s.id }
-
-      val hackathonSponsors: Map[Long, Iterable[Long]] =
-        model.Hackathon.all.toList.map({ h =>
-          (
-            h.id,
-            h.sponsors.map { s => s.id })
-        }).toMap
-
-      Ok(views.html.sponsors.index(model.Hackathon.all, sponsors, generalSponsors, hackathonSponsors, request.user))
-
+      Ok(views.html.sponsors.index(model.Sponsor.all, request.user))
     }
   }
 
   def view(id: Long) = UserAwareAction { implicit request =>
     transaction {
-      val hs = model.Sponsor.hackathonsToSponsors.where(hs => hs.sponsorId === id).map({ hs => (hs.hackathonId, hs.order) }).toMap
-      Ok(views.html.sponsors.view(model.Sponsor.lookup(id), hs, request.user))
+      Ok(views.html.sponsors.view(model.Sponsor.lookup(id), request.user))
     }
   }
 
   def create = SecuredAction() { implicit request =>
     transaction {
-      Ok(views.html.sponsors.create(sponsorForm, model.Hackathon.all.toList, request.user))
-    }
-  }
-
-  def uploadLogo = UserAwareAction(parse.multipartFormData) { implicit request =>
-
-    var temporaryHandle = request.body.file("files").get
-    val temporaryFile = temporaryHandle.ref.file
-    val in = new FileInputStream(temporaryFile)
-    val bytes = new Array[Byte](temporaryFile.length.toInt);
-    in.read(bytes)
-    in.close()
-    val filename = temporaryHandle.filename
-    val cloudImageService = use[CloudImagePlugin].cloudImageService
-    val response = cloudImageService.upload(filename, bytes)
-
-
-    response match {
-      case success: CloudImageSuccessResponse =>
-
-        transaction {
-          val res = new Resource(success.url, success.publicId);
-          val newRes = Resource.insert(res)
-
-          val result = Ok(toJson((Seq(JsObject(List(
-            "url" -> JsString(newRes.url),
-            "resourceId" -> JsNumber(newRes.id)))))))
-
-          result.withHeaders(CONTENT_TYPE -> "text/plain")
-        }
-      case error: CloudImageErrorResponse =>
-
-        Logger.debug("Sponsor - cloudinaryService - error: " + error.message)
-        Ok(uploadError)
-    }
-
-  }
-
-  def getLogoDetails(id: Long) = UserAwareAction { implicit request =>
-
-    transaction {
-      val r = Resource.lookup(id).get
-      Ok(JsArray(Seq(JsObject(List(
-        "url" -> JsString(r.url),
-        "resourceId" -> JsNumber(r.id))))))
+      val sponsor = new model.Sponsor(None)
+      Ok(views.html.sponsors.create(sponsorForm.fill(sponsor), request.user))
     }
   }
 
   def save = SecuredAction() { implicit request =>
     sponsorForm.bindFromRequest.fold(
       errors => transaction {
-        BadRequest(views.html.sponsors.create(errors, model.Hackathon.all.toList, request.user))
-      }, sponsor => transaction {
-
+        BadRequest(views.html.sponsors.create(errors, request.user))
+      },
+      sponsor => transaction {
         model.Sponsor.insert(sponsor)
-        sponsor.hackathonsList.map { hs =>
-
-          if (hs.order != -1) {
-
-            val hackathon = model.Hackathon.lookup(hs.hackathonId).get
-            //TODO: handle case with no hackathon returned from db
-            val hsTable: model.HackathonSponsor = model.HackathonSponsor(hs.hackathonId, sponsor.id, hs.order)
-            sponsor.hackathons.associate(hackathon, hsTable)
-          }
-        }
-
-        Redirect(routes.Sponsor.index).flashing("status" -> "sponsors.added")
+        Redirect(routes.Sponsor.index).flashing("status" -> "added", "title" -> sponsor.name)
       })
   }
 
   def edit(id: Long) = SecuredAction() { implicit request =>
-    transaction {
+  	transaction {
       model.Sponsor.lookup(id).map { sponsor =>
-
-        val hackathons = model.Hackathon.all.toList
-
-        val hsList: List[model.HackathonSponsorHelper] =
-          hackathons.map({ hackathon =>
-            model.HackathonSponsorHelper(
-              hackathon.id,
-              model.Sponsor.findSponsorHackatonOrder(id, hackathon.id).getOrElse(-1))
-          }).toList
-
-        Ok(views.html.sponsors.edit(id, sponsorForm.fill(sponsor.copy(hackathonsList = hsList)), hackathons, request.user))
-
-      }.get
+        Ok(views.html.sponsors.edit(id, sponsorForm.fill(sponsor), request.user))
+      }.getOrElse {
+        // no news found
+        Redirect(routes.Sponsor.view(id)).flashing()
+      }
     }
   }
 
   def update(id: Long) = SecuredAction() { implicit request =>
     sponsorForm.bindFromRequest.fold(
       errors => transaction {
-        BadRequest(views.html.sponsors.edit(id, errors, model.Hackathon.all.toList, request.user))
+        BadRequest(views.html.sponsors.edit(id, errors, request.user))
       },
       sponsor => transaction {
-
-        model.Sponsor.lookup(id).map { oldSponsor =>
-
-          if (oldSponsor.logoResourceId.isDefined && sponsor.logoResourceId.isEmpty) {
-            val resId = oldSponsor.logoResourceId.get
-            model.Resource.lookup(resId) map { resource =>
-
-              model.Sponsor.update(id, sponsor)	
-              model.Resource.delete(resId)
-              val cloudImageService = use[CloudImagePlugin].cloudImageService
-              cloudImageService.destroy(resource.publicId)
-
-            }
-          } else {
-            model.Sponsor.update(id, sponsor)
-          }
-
-          model.Sponsor.update(id, sponsor)
-          sponsor.hackathonsList.map { hs =>
-
-            if (hs.order == -1)
-              model.Sponsor.deleteSponsorHackathon(id, hs.hackathonId)
-            else
-              model.Sponsor.updateSponsorHackathonOrder(id, hs.hackathonId, hs.order)
-          }
-        }
-        Redirect(routes.Sponsor.index).flashing("status" -> "sponsors.updated", "title" -> sponsor.name)
+        model.Sponsor.update(id, sponsor)
+        Redirect(routes.Sponsor.index).flashing("status" -> "updated", "title" -> sponsor.name)
       })
   }
 
   def delete(id: Long) = SecuredAction() { implicit request =>
     transaction {
-
-      // TODO squeryl cascade
-      model.Sponsor.lookup(id).map { sponsor =>
-        sponsor.hackathons.dissociateAll
-        model.Sponsor.delete(id)
-        sponsor.logoResourceId map { rId =>
-          model.Resource.lookup(rId) map { resource =>
-            model.Resource.delete(resource.id)
-            val cloudImageService = use[CloudImagePlugin].cloudImageService
-            cloudImageService.destroy(resource.publicId)
-          }
-        }
-      }
-
+      model.Sponsor.delete(id)
+      Redirect(routes.Sponsor.index).flashing("status" -> "deleted")
     }
-    Redirect(routes.Sponsor.index).flashing("status" -> "sponsors.deleted")
   }
 
+  def uploadLogo = UserAwareAction(parse.multipartFormData) { implicit request =>
+    var temporaryHandle = request.body.file("files").get
+    val temporaryFile = temporaryHandle.ref.file
+    
+    val in = new FileInputStream(temporaryFile)
+    val bytes = new Array[Byte](temporaryFile.length.toInt);
+    in.read(bytes)
+    in.close()
+    
+    val filename = temporaryHandle.filename
+    val response = use[CloudImagePlugin].cloudImageService.upload(filename, bytes)
+
+    response match {
+      case success: CloudImageSuccessResponse =>
+        transaction {
+          val resource = model.Resource(success.url, success.publicId);
+          model.Resource.insert(resource)
+          val result = Ok(toJson((Seq(JsObject(List(
+            "url" -> JsString(resource.url),
+            "resourceId" -> JsNumber(resource.id)))))))
+          result.withHeaders(CONTENT_TYPE -> "text/plain")
+        }
+      case error: CloudImageErrorResponse =>
+        Logger.debug("Sponsor - cloudinaryService - error: " + error.message)
+        Ok(uploadError)
+    }
+  }
+
+  def getLogoDetails(id: Long) = UserAwareAction { implicit request =>
+    transaction {
+      val r = model.Resource.lookup(id).get
+      Ok(JsArray(Seq(JsObject(List(
+        "url" -> JsString(r.url),
+        "resourceId" -> JsNumber(r.id))))))
+    }
+  }
 }
-
-
-
-
- 
