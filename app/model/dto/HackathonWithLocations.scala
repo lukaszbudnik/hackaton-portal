8eb3de87bbfff07abd0c3c007567d56f8ecc8cdc
bykes <daniel.bykowski@gmail.com>
2012-08-09 13:39:41 +0200

    format code

--- app/model/dto/HackathonWithLocations.scala
+++ app/model/dto/HackathonWithLocations.scala
@@ -5,31 +5,29 @@ import org.squeryl.dsl.ast.LogicalBoolean
 import model.Hackathon
 import model.Location
 
-case class HackathonWithLocations(var hackathon : model.Hackathon, var locations : Seq[model.Location]) {
-	def this(hackathon : model.Hackathon) = this(hackathon,  List[model.Location]())
+case class HackathonWithLocations(var hackathon: model.Hackathon, var locations: Seq[model.Location]) {
+  def this(hackathon: model.Hackathon) = this(hackathon, List[model.Location]())
 }
-
-
-
-
 
 object HackathonWithLocations {
- 
 
-  
-  def lookup(id : Long) =  hackathonLocations((h, l) => h.id === id).headOption
-   
-  def all() = hackathonLocations((h, l) => 1 === 1)
- 
-   private def hackathonLocations(whereLogic : (Hackathon, Location) => LogicalBoolean) : Iterable[HackathonWithLocations] = {
-    from(model.Hackathon.hackathons, model.Location.locations, model.Hackathon.hackathonLocations) ((h, l, hl) =>
-      where (h.id === hl.hackathonId and l.id === hl.locationId and whereLogic(h, l))
-      select (h, l))
+  private def hackathonLocations(whereLogic: (Hackathon, Location) => LogicalBoolean): Iterable[HackathonWithLocations] = {
+    from(model.Hackathon.hackathons, model.Location.locations, model.Hackathon.hackathonLocations)((h, l, hl) =>
+      where(h.id === hl.hackathonId and l.id === hl.locationId and whereLogic(h, l))
+        select (h, l))
       .groupBy(x => x._1)
-      .map { 
-    	x => 
-    	  new HackathonWithLocations(x._1, x._2.map {y => y._2}.toSeq)} 
+      .map {
+        x =>
+          new HackathonWithLocations(x._1, x._2.map { y => y._2 }.toSeq)
+      }
   }
- 
-}
 
+  def lookup(id: Long) = {
+    hackathonLocations((h, l) => h.id === id).headOption
+  }
+
+  def all() = {
+    hackathonLocations((h, l) => 1 === 1)
+  }
+
+}
--- app/model/dto/SponsorWithLogo.scala
+++ app/model/dto/SponsorWithLogo.scala
@@ -5,31 +5,31 @@ import model.Resource
 import org.squeryl.dsl.ast.LogicalBoolean
 import org.squeryl.PrimitiveTypeMode._
 
-case class SponsorWithLogo(sponsor : Sponsor, logo : Option[Resource]) {
-	def this() = this(new Sponsor(), None)
+case class SponsorWithLogo(sponsor: Sponsor, logo: Option[Resource]) {
+  def this() = this(new Sponsor(), None)
 }
 
-object SponsorWithLogo { 
+object SponsorWithLogo {
 
- def lookup(id: Long) : Option[SponsorWithLogo] = {
-	sponsorsWithLogo((s,r) => s.id === id).headOption
-  }
- 
-  def hackathonSponsors(hackathonId : Long) : Seq[SponsorWithLogo] = {
-    sponsorsWithLogo((s,r) => (s.hackathonId === hackathonId))
-  }
-  
-  def portalSponsors() : Seq[SponsorWithLogo] = {
-    sponsorsWithLogo( (s,r) => 1 === 1)
-  }
-  
-  private def sponsorsWithLogo(whereLogic : (Sponsor, Option[Resource]) => LogicalBoolean) : Seq[SponsorWithLogo] = {
-    join(model.Sponsor.sponsors, model.Resource.resources.leftOuter)((s,r) =>
-      where(whereLogic(s,r))
+  private def sponsorsWithLogo(whereLogic: (Sponsor, Option[Resource]) => LogicalBoolean): Seq[SponsorWithLogo] = {
+    join(model.Sponsor.sponsors, model.Resource.resources.leftOuter)((s, r) =>
+      where(whereLogic(s, r))
         select (s, r)
         orderBy (s.order)
         on (s.logoResourceId === r.map(_.id)))
-        .toSeq.map { t => new SponsorWithLogo(t._1, t._2) }.toSeq
+      .toSeq.map { t => new SponsorWithLogo(t._1, t._2) }.toSeq
   }
- 
+  
+  def lookup(id: Long): Option[SponsorWithLogo] = {
+    sponsorsWithLogo((s, r) => s.id === id).headOption
+  }
+
+  def hackathonSponsors(hackathonId: Long): Seq[SponsorWithLogo] = {
+    sponsorsWithLogo((s, r) => (s.hackathonId === hackathonId))
+  }
+
+  def portalSponsors(): Seq[SponsorWithLogo] = {
+    sponsorsWithLogo((s, r) => 1 === 1)
+  }
+
 }
