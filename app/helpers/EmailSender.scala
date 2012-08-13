a4aeb95dea8a7e4ca77f8d90b746d0a4f1db0abb
bykes <daniel.bykowski@gmail.com>
2012-08-13 11:25:46 +0200

    [#33923397] fix email parameters and use html messages

--- app/helpers/EmailSender.scala
+++ app/helpers/EmailSender.scala
@@ -15,34 +15,34 @@ object EmailSender {
   private val emailNotifierService = use[EmailNotifierPlugin].emailNotifierService
 
   def sendEmailToHackathonOrganiser(hackathon: Hackathon, subject: String, body: String, params: Seq[String] = Seq()) = {
-    val mailBody = Messages(body, params)
-    val mailSubject = Messages(subject, params)
+    val mailBody = Messages(body, params:_*)
+    val mailSubject = Messages(subject, params:_*)
 
-    emailNotifierService.send(from, hackathon.organiser.email, mailSubject, mailBody)
+    emailNotifierService.send(from, hackathon.organiser.email, mailSubject, mailBody, Some(mailBody))
   }
 
   def sendEmailToProblemSubmitter(problem: Problem, subject: String, body: String, params: Seq[String] = Seq()) = {
-    val mailBody = Messages(body, params)
-    val mailSubject = Messages(subject, params)
+    val mailBody = Messages(body, params:_*)
+    val mailSubject = Messages(subject, params:_*)
 
-    emailNotifierService.send(from, problem.submitter.email, mailSubject, mailBody)
+    emailNotifierService.send(from, problem.submitter.email, mailSubject, mailBody, Some(mailBody))
   }
   
   def sendEmailToTeamCreator(team: Team, subject: String, body: String, params: Seq[String] = Seq()) = {
-    val mailBody = Messages(body, params)
-    val mailSubject = Messages(subject, params)
+    val mailBody = Messages(body, params:_*)
+    val mailSubject = Messages(subject, params:_*)
 
-    emailNotifierService.send(from, team.creator.email, mailSubject, mailBody)
+    emailNotifierService.send(from, team.creator.email, mailSubject, mailBody, Some(mailBody))
   }
 
   def sendEmailToWholeTeam(team: Team, subject: String, body: String, params: Seq[String] = Seq()) = {
-    val mailBody = Messages(body, params)
-    val mailSubject = Messages(subject, params)
+    val mailBody = Messages(body, params:_*)
+    val mailSubject = Messages(subject, params:_*)
 
     val emails = Seq(team.creator.email) ++ team.members.map { _.email }
 
     emails.distinct.map { to =>
-      emailNotifierService.send(from, to, mailSubject, mailBody)
+      emailNotifierService.send(from, to, mailSubject, mailBody, Some(mailBody))
     }
   }
 
