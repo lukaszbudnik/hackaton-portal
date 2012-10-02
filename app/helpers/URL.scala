d35a6ada05c2dacff1e3d00a67c80a2b8ac6148b
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-10-02 14:57:06 +0200

    a few code tweaks

--- app/helpers/URL.scala
+++ app/helpers/URL.scala
@@ -1,11 +1,25 @@
 package helpers
+
 import play.api.mvc.Call
 import play.api.Play
+import play.api.Application
+import play.api.Mode
 
 object URL {
-  
-  private val applicationUrl = Play.current.configuration.getString("application.url").get
-  
+
+  private val applicationUrl = {
+    Play.current.mode match {
+      case Mode.Prod =>
+        val baseUrl = Play.current.configuration.getString("application.url").get
+        if (baseUrl.endsWith("/")) {
+          baseUrl.substring(0, baseUrl.length() - 1)
+        } else {
+          baseUrl
+        }
+      case _ => "http://localhost:9000"
+    }
+  }
+
   def externalUrl(call: Call) = {
     applicationUrl + call.url
   }
