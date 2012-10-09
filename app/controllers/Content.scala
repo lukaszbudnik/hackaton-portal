5db4d458baaa84ca84a48f3cc72b96fb5566ad57
lukgad <l.gadomski@kainos.com>
2012-10-09 16:00:31 +0200

    First view - display entries from Content controller

--- app/controllers/Content.scala
+++ app/controllers/Content.scala
@@ -1,11 +1,16 @@
 package controllers
 
 import play.mvc.Controller
-
 import play.api.mvc.Action
+import cms.ContentManager
 
 object Content extends core.LangAwareController {
 	def index = Action{
-	  Ok("Test demo")
+	  var entityList = ContentManager.all	  
+	  Ok(views.html.content.index(entityList))
+	}
+	
+	def view(key: String) = Action{
+	  Ok("in progress, in future will fetch details for entry "+key)
 	}
 }
