73bc255514f561a326c4d64c2ce6d18b0cda1f83
lukgad <l.gadomski@kainos.com>
2012-10-10 10:50:30 +0200

    Added simple readonly form for displaying Entry details

--- app/controllers/Content.scala
+++ app/controllers/Content.scala
@@ -3,15 +3,18 @@ package controllers
 import play.mvc.Controller
 import play.api.mvc.Action
 import cms.ContentManager
+import cms.dto.Entry
 
 object Content extends core.LangAwareController {
+
 	def index = Action{
 	  var entityList = ContentManager.all	  
 	  Ok(views.html.content.index(entityList))
 	}
 	
-	def view(key: String) = Action{
-	  var entry = ContentManager.find(key).get
-	  Ok(views.html.content.view(entry))
+	//displays details of cms.dto.Entry in view only mode
+	def view(key: String, mode: String) = Action{
+	  var entry = ContentManager.find(key)
+	  Ok(views.html.content.view(entry,mode))
 	}
 }
