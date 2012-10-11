dd773a9958d84864e7466bfac253b8a295cd585c
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-10-11 09:33:35 +0200

    refactored code added support for create/save

--- app/controllers/Content.scala
+++ app/controllers/Content.scala
@@ -10,50 +10,56 @@ import play.api.data.validation.Constraints._
 import org.squeryl.PrimitiveTypeMode.transaction
 object Content extends core.LangAwareController {
 
-	//Entry form
-	val entryForm = Form(
-	  mapping(
-	    "key" -> nonEmptyText,
-	    "entryType" -> helpers.Forms.enum(cms.dto.EntryType),
-	    "content" -> list(mapping(
-	        "lang" -> text,
-	        "value" -> text
-	        )(cms.dto.Content.apply)(cms.dto.Content.unapply)
-	        )
-	  )(Entry.apply)//((key, entryType) => Entry(key,entryType, List.empty))
-	  (Entry.unapply)//((entry: Entry) => Some(entry.key, entry.entryType))
-	)
-	
-	//lists all entries
-	def index = Action{
-	  var entityList = ContentManager.all	  
-	  Ok(views.html.content.index(entityList))
-	}
-	
-	//displays details of cms.dto.Entry in edit mode
-	def view(key: String) = Action{
-	  var entry = ContentManager.find(key)
-	  Ok(views.html.content.view(entry,true))
-	}
-	
-	//displays page for adding new Entry 
-	def create = Action {
-		var option : Option[Entry] = Some(Entry("", cms.dto.EntryType.HTML, List.empty))		
-		Ok(views.html.content.view(option,false))
-	}
-	
-	//updates Entry with values from form
-	def update(key: String) = Action{ implicit request => 
-	  entryForm.bindFromRequest.fold(
-	      errors => transaction {BadRequest(views.html.content.index(Nil))}, //TODO: handle bad request
-	      entry => transaction {
-	        ContentManager.update(entry)
-	      }
-	  )
-	  Redirect(routes.Content.index)
-	}
-	
-	def save = Action {
-	  TODO
-	}
+  val entryForm = Form(
+    mapping(
+      "key" -> nonEmptyText,
+      "entryType" -> helpers.Forms.enum(cms.dto.EntryType),
+      "content" -> list(mapping(
+        "lang" -> text,
+        "value" -> text)(cms.dto.Content.apply)(cms.dto.Content.unapply)))(Entry.apply)(Entry.unapply))
+
+  def index = Action {
+    val entityList = ContentManager.all
+    Ok(views.html.content.index(entityList))
+  }
+
+  def create = Action {
+    val entry = Entry("", cms.dto.EntryType.HTML, List.empty)
+    Ok(views.html.content.create(entryForm.fill(entry)))
+  }
+
+  def save = Action { implicit request =>
+    entryForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.content.create(errors)),
+      entry => {
+        ContentManager.create(entry)
+        Redirect(routes.Content.index)
+      })
+  }
+
+  def edit(key: String) = Action {
+    val entry = ContentManager.find(key)
+    entry.map { entry =>
+      Ok(views.html.content.edit(key, entryForm.fill(entry)))
+    }.getOrElse {
+      Redirect(routes.Content.index)
+    }
+  }
+
+  def update(key: String) = Action { implicit request =>
+    val entry = ContentManager.find(key)
+    entry.map { entry =>
+      entryForm.bindFromRequest.fold(
+        errors => BadRequest(views.html.content.edit(key, errors)),
+        entry => {
+          ContentManager.update(entry)
+          Redirect(routes.Content.index)
+        })
+    }.getOrElse {
+      Redirect(routes.Content.index)
+    }
+  }
+
+  def delete(key: String) = TODO
+
 }
