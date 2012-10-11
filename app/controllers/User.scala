2d1498a63e77044b17aa3ba6b51ffca3e38e2e9f
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-10-11 08:24:39 +0200

    cleaned up empty lines

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -14,66 +14,65 @@ import helpers.Security
 import core.LangAwareController
 
 object User extends LangAwareController with securesocial.core.SecureSocial {
-  
+
   private val PAGE_SIZE = 20
 
   def index(page: Int, orderBy: Int, filter: String) = SecuredAction() { implicit request =>
     transaction {
-      
+
       implicit val socialUser = request.user
       Security.verifyIfAllowed(socialUser)
-      
+
       val offset = PAGE_SIZE * page
-      
+
       val totalUsers = model.User.pagedUsersTotalNumber(filter)
-      
+
       val users = model.User.pagedUsers(orderBy, filter, offset, PAGE_SIZE)
-      
+
       val currentPage = Page(users, page, offset, totalUsers)
-      
+
       Ok(views.html.users.index(currentPage, socialUser, orderBy, filter))
     }
   }
 
   def updateIsAdmin(userId: Int, isAdmin: Boolean) = SecuredAction() { implicit request =>
     transaction {
-      
+
       implicit val socialUser = request.user
       Security.verifyIfAllowed(socialUser)
-      
+
       val user = model.User.lookup(userId)
-      
+
       user.map { u =>
         val userToBeUpdated = u.copy(isAdmin = isAdmin)
         model.User.update(userId, userToBeUpdated)
         Ok(JsArray(Seq(JsObject(List(
-        	"status" -> JsString("ok"))))))
+          "status" -> JsString("ok"))))))
       }.getOrElse {
         NotFound(JsArray(Seq(JsObject(List(
-        	"status" -> JsString("error"))))))
-      }
-    }
-  }
-    
-  def updateIsBlocked(userId: Int, isBlocked: Boolean) = SecuredAction() { implicit request =>
-    transaction {
-      
-      implicit val socialUser = request.user
-      Security.verifyIfAllowed(socialUser)
-      
-      val user = model.User.lookup(userId)
-      
-      user.map { u =>
-        val userToBeUpdated = u.copy(isBlocked = isBlocked)
-        model.User.update(userId, userToBeUpdated)
-        Ok(JsArray(Seq(JsObject(List(
-        	"status" -> JsString("ok"))))))
-      }.getOrElse {
-        NotFound(JsArray(Seq(JsObject(List(
-        	"status" -> JsString("error"))))))
+          "status" -> JsString("error"))))))
       }
     }
   }
 
+  def updateIsBlocked(userId: Int, isBlocked: Boolean) = SecuredAction() { implicit request =>
+    transaction {
+
+      implicit val socialUser = request.user
+      Security.verifyIfAllowed(socialUser)
+
+      val user = model.User.lookup(userId)
+
+      user.map { u =>
+        val userToBeUpdated = u.copy(isBlocked = isBlocked)
+        model.User.update(userId, userToBeUpdated)
+        Ok(JsArray(Seq(JsObject(List(
+          "status" -> JsString("ok"))))))
+      }.getOrElse {
+        NotFound(JsArray(Seq(JsObject(List(
+          "status" -> JsString("error"))))))
+      }
+    }
+  }
 
 }
