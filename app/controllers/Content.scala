5266d850d598ef5eeba8f3affc8d317707371b11
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-10-22 14:15:02 +0200

    secured all actions, only admins can access CMS sub system

--- app/controllers/Content.scala
+++ app/controllers/Content.scala
@@ -8,6 +8,7 @@ import play.api.data._
 import play.api.data.validation.Constraints._
 import org.squeryl.PrimitiveTypeMode.transaction
 import play.api.mvc.Action
+import helpers.Security
 
 object Content extends core.LangAwareController with securesocial.core.SecureSocial {
 
@@ -19,18 +20,30 @@ object Content extends core.LangAwareController with securesocial.core.SecureSoc
         "lang" -> text,
         "value" -> text)(cms.dto.Content.apply)(cms.dto.Content.unapply)))(Entry.apply)(Entry.unapply))
 
-  def index = Action { implicit request =>
+  def index = SecuredAction() { implicit request =>
+
+    implicit val socialUser = request.user
+    Security.verifyIfAllowed(socialUser)
+
     val entityList = ContentManager.all
-    
+
     Ok(views.html.contents.index(entityList, None))
   }
 
-  def create = Action { implicit request =>
+  def create = SecuredAction() { implicit request =>
+
+    implicit val socialUser = request.user
+    Security.verifyIfAllowed(socialUser)
+
     val entry = Entry("", cms.dto.EntryType.HTML, List.empty)
     Ok(views.html.contents.create(entryForm.fill(entry), None))
   }
 
-  def save = Action { implicit request =>
+  def save = SecuredAction() { implicit request =>
+
+    implicit val socialUser = request.user
+    Security.verifyIfAllowed(socialUser)
+
     entryForm.bindFromRequest.fold(
       errors => BadRequest(views.html.contents.create(errors, None)),
       entry => {
@@ -40,7 +53,11 @@ object Content extends core.LangAwareController with securesocial.core.SecureSoc
 
   }
 
-  def edit(key: String) = Action { implicit request =>
+  def edit(key: String) = SecuredAction() { implicit request =>
+
+    implicit val socialUser = request.user
+    Security.verifyIfAllowed(socialUser)
+
     val entry = ContentManager.find(key)
     entry.map { entry =>
       Ok(views.html.contents.edit(key, entryForm.fill(entry), None))
@@ -50,7 +67,11 @@ object Content extends core.LangAwareController with securesocial.core.SecureSoc
 
   }
 
-  def update(key: String) = Action { implicit request =>
+  def update(key: String) = SecuredAction() { implicit request =>
+
+    implicit val socialUser = request.user
+    Security.verifyIfAllowed(socialUser)
+
     val entry = ContentManager.find(key)
     entry.map { entry =>
       entryForm.bindFromRequest.fold(
@@ -65,7 +86,11 @@ object Content extends core.LangAwareController with securesocial.core.SecureSoc
 
   }
 
-  def delete(key: String) = Action { implicit request =>
+  def delete(key: String) = SecuredAction() { implicit request =>
+
+    implicit val socialUser = request.user
+    Security.verifyIfAllowed(socialUser)
+
     val entry = ContentManager.find(key)
     entry.map { entry =>
       ContentManager.remove(entry)
