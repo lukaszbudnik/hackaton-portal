338fc1244a45987749e113354b2dc050ab98c95b
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-11-13 13:47:14 +0100

    updated github name for Łukasz Budnik

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups 
 
-insert into users (name, email, github_username, open_id, avatar_url, is_admin) values ('Łukasz Budnik','l.budnik@kainos.com', 'lukasz-budnik', '108382012254355503021google', 'https://lh4.googleusercontent.com/--E57EZJfFuE/AAAAAAAAAAI/AAAAAAAADMk/4soRdJYOuoQ/s250-c-k/photo.jpg', true);
+insert into users (name, email, github_username, open_id, avatar_url, is_admin) values ('Łukasz Budnik','l.budnik@kainos.com', 'lukaszbudnik', '108382012254355503021google', 'https://lh4.googleusercontent.com/--E57EZJfFuE/AAAAAAAAAAI/AAAAAAAADMk/4soRdJYOuoQ/s250-c-k/photo.jpg', true);
 insert into users (name, email, github_username, open_id, avatar_url, is_admin) values ('Daniel Bykowski','d.bykowski@kainos.com', 'bykes', '102068228977340999495google', 'https://lh4.googleusercontent.com/-_qzaBoetBfc/AAAAAAAAAAI/AAAAAAAAENc/q3rBQpasMfI/photo.jpg', true);
 
 # --- !Downs
