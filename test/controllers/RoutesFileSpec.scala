2d48d4aaec58c92ee1a084cfa229b968cccdb148
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-11-16 11:07:09 +0100

    used RoutesFileSpec to detect simple HTML parse errors, fixed all errors caused by programmers, there are still errors caused by Play templates (like not closed <input> elements)

--- test/controllers/RoutesFileSpec.scala
+++ test/controllers/RoutesFileSpec.scala
@@ -11,78 +11,72 @@ import scala.collection.Map
 import scala.xml.XML
 import play.api.Play
 
-
 class RoutesFileSpec extends Specification {
-
+  
   val idMap = Map("/locations/:id" -> "/locations/1",
-		  			"/hackathons/:id/:uid" -> "/hackathons/1/1",
-		  			"/hackathons/:id" ->  "/hackathons/1",
-		  			"/hackathons/:hid" -> "/hackathons/1",
-		  			"/hackathons/:hid/prizes" -> "/hackathons/1/prizes",
-		  			"/changeLanguage/:lang" -> "/changeLanguage/PL",
-		  			"/news/:id" -> "/news/1",
-		  			"/search/:label" -> "/search/string",
-		  			"/hackathon.json/:id" -> "/hackathon.json/1",
-		  			"/teams/:id" -> "/teams/1",
-		  			"/prizes/:id" -> "/prizes/1",
-		  			"/sponsors/:id" -> "/sponsors/1",
-		  			"/authenticate/:provider" -> "/authenticate/google",
-		  			"/contents/:key" -> "/contents/1",
-		  			"chat/:id" -> "chat/1",
-		  			"/:uid" -> "/1",
-		  			"/problems/:id" -> "/problems/1",
-		  			"""/assets/\*file""" -> "/assets/images/favicon.png",
-		  			"uploadLogo/:id" -> "uploadLogo/1" 
-		  			
-		  		 )
+    "/hackathons/:id/:uid" -> "/hackathons/1/1",
+    "/hackathons/:id" -> "/hackathons/1",
+    "/hackathons/:hid" -> "/hackathons/1",
+    "/hackathons/:hid/prizes" -> "/hackathons/1/prizes",
+    "/changeLanguage/:lang" -> "/changeLanguage/PL",
+    "/news/:id" -> "/news/1",
+    "/search/:label" -> "/search/string",
+    "/hackathon.json/:id" -> "/hackathon.json/1",
+    "/teams/:id" -> "/teams/1",
+    "/prizes/:id" -> "/prizes/1",
+    "/sponsors/:id" -> "/sponsors/1",
+    "/authenticate/:provider" -> "/authenticate/google",
+    "/contents/:key" -> "/contents/1",
+    "chat/:id" -> "chat/1",
+    "/:uid" -> "/1",
+    "/problems/:id" -> "/problems/1",
+    """/assets/\*file""" -> "/assets/images/favicon.png",
+    "uploadLogo/:id" -> "uploadLogo/1")
 
   "entry in routes file" >> {
-    
-    parse foreach { tuple => 
+
+    parse foreach { tuple =>
       ("action renders correctly: " + tuple) >> {
         val fakeApp = FakeApplication(additionalConfiguration = inMemoryDatabase())
         running(fakeApp) {
           val httpMethod = tuple._1
-        	var action = tuple._2
-            
-        	
-        	val skipAll = Play.current.configuration.getString("mongodb.uri").get == "mock"
-            
-        	  
-        	// skipping tests  
-        	if (skipAll && (action.startsWith("/contents") || action.startsWith("/messages.js"))) {
-              skipped("skipping test for mock mongodb")
-            } 
-            		  
-        	
-        	idMap.map(id => action = action.replaceAll(id._1, id._2))
-        	
-        	
-        	
-        	val result = SecureSocialUtils.fakeAuth(FakeRequest(httpMethod, action), fakeApp)
-        	status(result) must beOneOf(OK, SEE_OTHER)     
-        	
+          var action = tuple._2
+
+          val mongoMock = Play.current.configuration.getString("mongodb.uri").get == "mock"
+
+          if (mongoMock && (action.startsWith("/contents") || action.startsWith("/messages.js"))) {
+            skipped("skipping test for mock mongodb")
+          }
+
+          idMap.map(id => action = action.replaceAll(id._1, id._2))
+
+          val result = SecureSocialUtils.fakeAuth(FakeRequest(httpMethod, action), fakeApp)
+          
+//          contentType(result) match {
+//            case Some(contentType: String) if contentType == "text/html" || contentType == "application/xml" => XML.loadString(contentAsString(result).replaceAll("&", "&#38;"))
+//            case _ =>
+//          }
+          
+          status(result) must beOneOf(OK, SEE_OTHER)
 
         }
       }
-      
+
     }
-      
-    
+
   }
 
-  def parse : Iterator[(String, String)] = {
-    
+  def parse: Iterator[(String, String)] = {
+
     val regex = """(\w+)\s+([^\s]+)\s+(.+)""".r
-    val lines: Iterator[String] = scala.io.Source.fromFile("conf/routes").getLines.filter((l: String) => (!l.startsWith("#") && !l.startsWith("POST") && l.trim().length() > 0))  
-    lines.map(line =>  line.trim() match {
-        case regex(httpM, action, contrMeth) => (httpM.trim(), action.trim())
-        case _ => {
-          println(line) 
-          throw new IllegalArgumentException("routes file format incorrect")
-          }
-      } )
-      
+    val lines: Iterator[String] = scala.io.Source.fromFile("conf/routes").getLines.filter((l: String) => (!l.startsWith("#") && !l.startsWith("POST") && l.trim().length() > 0))
+    lines.map(line => line.trim() match {
+      case regex(httpM, action, contrMeth) => (httpM.trim(), action.trim())
+      case _ => {
+        throw new IllegalArgumentException("routes file format incorrect")
+      }
+    })
+
   }
 
 }
