acebcfe366e999bbb3941c212ecc3bdac698e915
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-11-19 18:10:53 +0100

    down to 14 compile errors

--- app/controllers/News.scala
+++ app/controllers/News.scala
@@ -24,25 +24,25 @@ object News extends LangAwareController with securesocial.core.SecureSocial {
 
   def index = UserAwareAction { implicit request =>
     transaction {
-      Ok(views.html.news.index(model.News.all, request.user))
+      Ok(views.html.news.index(model.News.all, userFromRequest))
     }
   }
 
   def search(label: String) = UserAwareAction { implicit request =>
     transaction {
-      Ok(views.html.news.index(model.News.findByLabel(label), request.user, label))
+      Ok(views.html.news.index(model.News.findByLabel(label), userFromRequest, label))
     }
   }
 
   def indexH(hid: Long) = UserAwareAction { implicit request =>
     transaction {
-      Ok(views.html.news.indexH(model.Hackathon.lookup(hid), request.user))
+      Ok(views.html.news.indexH(model.Hackathon.lookup(hid), userFromRequest))
     }
   }
 
   def view(id: Long) = UserAwareAction { implicit request =>
     transaction {
-      Ok(views.html.news.view(model.News.lookup(id), request.user))
+      Ok(views.html.news.view(model.News.lookup(id), userFromRequest))
     }
   }
 
@@ -50,73 +50,70 @@ object News extends LangAwareController with securesocial.core.SecureSocial {
     transaction {
       val news = model.News.lookup(id)
       val hackathon = news.map { news => news.hackathon }.getOrElse { model.Hackathon.lookup(hid) }
-      Ok(views.html.news.viewH(hackathon, news, request.user))
+      Ok(views.html.news.viewH(hackathon, news, userFromRequest))
     }
   }
 
   def create = SecuredAction() { implicit request =>
     helpers.Security.verifyIfAllowed(request.user)
     transaction {
-      model.User.lookupByOpenId(request.user.id.id + request.user.id.providerId).map { user =>
-        val news = new model.News(user.id)
-        Ok(views.html.news.create(newsForm.fill(news), request.user))
-      }.getOrElse(Redirect(routes.News.index))
+      val user = userFromRequest(request)
+      val news = new model.News(user.id)
+      Ok(views.html.news.create(newsForm.fill(news), user))
     }
   }
 
   def createH(hid: Long) = SecuredAction() { implicit request =>
+    val hackathon = model.Hackathon.lookup(hid)
+    hackathon.map { hackathon =>
+      helpers.Security.verifyIfAllowed(hackathon.organiserId)(request.user)
+    }
     transaction {
-      val hackathon = model.Hackathon.lookup(hid)
-      hackathon.map { hackathon =>
-        helpers.Security.verifyIfAllowed(hackathon.organiserId)(request.user)
-      }
-      model.User.lookupByOpenId(request.user.id.id + request.user.id.providerId).map { user =>
+      val user = userFromRequest(request)
       val news = new model.News(user.id, Some(hid))
-      Ok(views.html.news.createH(hackathon, newsForm.fill(news), request.user))
-      }.getOrElse(Redirect(routes.News.indexH(hid)))
+      Ok(views.html.news.createH(hackathon, newsForm.fill(news), user))
     }
   }
 
   def save = SecuredAction() { implicit request =>
     helpers.Security.verifyIfAllowed(request.user)
+    val user = userFromRequest(request)
     newsForm.bindFromRequest.fold(
-      errors => transaction {
-        BadRequest(views.html.news.create(errors, request.user))
-      },
+      errors => BadRequest(views.html.news.create(errors, user)),
       news => transaction {
-        helpers.Security.verifyIfAllowed(request.user)
         model.News.insert(news)
         Redirect(routes.News.index).flashing("status" -> "added", "title" -> news.title)
       })
   }
 
   def saveH(hid: Long) = SecuredAction() { implicit request =>
+    val user = userFromRequest(request)
+    model.Hackathon.lookup(hid).map { hackathon =>
+      helpers.Security.verifyIfAllowed(hackathon.organiserId)(request.user)
+    }
     newsForm.bindFromRequest.fold(
-      errors => transaction {
-        BadRequest(views.html.news.createH(model.Hackathon.lookup(hid), errors, request.user))
-      },
+      errors => BadRequest(views.html.news.createH(model.Hackathon.lookup(hid), errors, user)),
       news => transaction {
-        model.Hackathon.lookup(hid).map { hackathon =>
-          helpers.Security.verifyIfAllowed(hackathon.organiserId)(request.user)
-        }
         model.News.insert(news)
         Redirect(routes.News.indexH(hid)).flashing("status" -> "added", "title" -> news.title)
       })
   }
 
   def edit(id: Long) = SecuredAction() { implicit request =>
+    val user = userFromRequest(request)
     transaction {
       model.News.lookup(id).map { news =>
         helpers.Security.verifyIfAllowed(news.authorId)(request.user)
-        Ok(views.html.news.edit(id, newsForm.fill(news.copy(labelsAsString = news.labels.map(_.value).mkString(","))), request.user))
+        Ok(views.html.news.edit(id, newsForm.fill(news.copy(labelsAsString = news.labels.map(_.value).mkString(","))), user))
       }.getOrElse {
         // no news found
-        Redirect(routes.News.view(id)).flashing()
+        Redirect(routes.News.view(id))
       }
     }
   }
 
   def editH(hid: Long, id: Long) = SecuredAction() { implicit request =>
+    val user = userFromRequest(request)
     transaction {
       model.News.lookup(id).map { news =>
         helpers.Security.verifyIfAllowed(Some(hid) == news.hackathonId)(request.user)
@@ -125,7 +122,7 @@ object News extends LangAwareController with securesocial.core.SecureSocial {
         }.getOrElse {
           helpers.Security.verifyIfAllowed(news.authorId)(request.user)
         }
-        Ok(views.html.news.editH(news.hackathon, id, newsForm.fill(news.copy(labelsAsString = news.labels.map(_.value).mkString(","))), request.user))
+        Ok(views.html.news.editH(news.hackathon, id, newsForm.fill(news.copy(labelsAsString = news.labels.map(_.value).mkString(","))), user))
       }.getOrElse {
         // no news found
         Redirect(routes.News.viewH(hid, id)).flashing()
@@ -134,10 +131,9 @@ object News extends LangAwareController with securesocial.core.SecureSocial {
   }
 
   def update(id: Long) = SecuredAction() { implicit request =>
+    val user = userFromRequest(request)
     newsForm.bindFromRequest.fold(
-      errors => transaction {
-        BadRequest(views.html.news.edit(id, errors, request.user))
-      },
+      errors => BadRequest(views.html.news.edit(id, errors, user)),
       news => transaction {
         model.News.lookup(id).map { news =>
           helpers.Security.verifyIfAllowed(news.authorId)(request.user)
@@ -148,10 +144,9 @@ object News extends LangAwareController with securesocial.core.SecureSocial {
   }
 
   def updateH(hid: Long, id: Long) = SecuredAction() { implicit request =>
+    val user = userFromRequest(request)
     newsForm.bindFromRequest.fold(
-      errors => transaction {
-        BadRequest(views.html.news.editH(model.Hackathon.lookup(hid), id, errors, request.user))
-      },
+      errors => BadRequest(views.html.news.editH(model.Hackathon.lookup(hid), id, errors, user)),
       news => transaction {
         model.News.lookup(id).map { news =>
           helpers.Security.verifyIfAllowed(Some(hid) == news.hackathonId)(request.user)
