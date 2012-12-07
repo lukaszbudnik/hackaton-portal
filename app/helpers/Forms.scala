02c5563df376d85179960ac5b9bd02f28229fc48
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-12-07 07:34:39 +0100

    added new allowed tags, added Form mapping of simpleHtmlOnly

--- app/helpers/Forms.scala
+++ app/helpers/Forms.scala
@@ -10,6 +10,8 @@ object Forms {
   val real: Mapping[Double] = of[Double]
   
   val nonEmptyTextNonHtml: Mapping[String] = of[String] verifying (play.api.data.validation.Constraints.nonEmpty, helpers.Constraints.nonHtml) 
+  
+  val nonEmptyTextSimpleHtmlOnly: Mapping[String] = of[String] verifying (play.api.data.validation.Constraints.nonEmpty, helpers.Constraints.simpleHtmlOnly)
 
   def enum[E <: Enumeration](enum: E): Mapping[E#Value] = of(enumFormat(enum))
 }
