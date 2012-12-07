8c510cf067f549b723794148bf8b12a018a202ea
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-12-07 19:32:37 +0100

    new messages for forms errors, plus added translation for Polish language

---
+++ conf/messages.pl
@@ -0,0 +1,28 @@
+# Default messages translated to Polish
+
+# --- Constraints
+constraint.required=Wymagane
+constraint.min=Wartość minimalna: {0}
+constraint.max=Wartość maksymalna: {0}
+constraint.minLength=Minimalna długość: {0}
+constraint.maxLength=Maksymalna długość: {0}
+constraint.email=Email
+
+# --- Formats
+format.date=Data (''{0}'')
+format.numeric=Numeric
+
+# --- Errors
+error.invalid=Niepoprawa wartość
+error.required=Pole wymagane
+error.number=Oczekiwano wartości numerycznej
+error.min=Musi być większe lub równe {0}
+error.max=Musi być mniejsze lub równe {0}
+error.minLength=Minimalna długość to {0}
+error.maxLength=Maksymalna długość to {0}
+error.email=Poprawny email jest wymagany
+error.pattern=Musi pasować do {0}
+
+# --- Hackaton.pl specific messages
+error.nonHtml = Tagi HTML niedozwolone
+error.simpleHtmlOnly = Tylko proste tagi HTML są dozwolone: {0}
