184392631079bfa2a8bf1d787251cbe4549d5676
Łukasz Budnik <lukasz.budnik@gmail.com>
2012-12-09 09:55:54 +0100

    amended tests, added checking with db

--- test/controllers/NewsSpec.scala
+++ test/controllers/NewsSpec.scala
@@ -5,6 +5,7 @@ import play.api.test._
 import play.api.test.Helpers._
 import play.api.i18n.Messages
 import play.api.test.FakeRequest$
+import org.squeryl.PrimitiveTypeMode.transaction
 
 class NewsSpec extends Specification {
 
@@ -18,16 +19,20 @@ class NewsSpec extends Specification {
         contentAsString(result) must contain(helpers.CmsMessages("news.title"))
       }
     }
-    
+
     "display news on GET /news/1" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        val result = routeAndCall(FakeRequest(GET, "/news/1")).get
+        transaction {
+          val dbNews = model.News.lookup(1).get
 
-        status(result) must equalTo(OK)
-        contentAsString(result) must contain(helpers.CmsMessages("news.title"))
+          val result = routeAndCall(FakeRequest(GET, "/news/1")).get
+
+          status(result) must equalTo(OK)
+          contentAsString(result) must contain(dbNews.title)
+        }
       }
     }
-    
+
     "redirect to Login Page on GET /news/new (controllers.News.create)" in {
       // application.secret is required for redirections!
       running(FakeApplication(additionalConfiguration = inMemoryDatabase() + (("application.secret", "asasasas")))) {
@@ -37,7 +42,7 @@ class NewsSpec extends Specification {
         redirectLocation(result) must beSome.which(_ == "/login")
       }
     }
-    
+
     "redirect to Login Page on POST /news (controllers.News.save)" in {
       // application.secret is required for redirections!
       running(FakeApplication(additionalConfiguration = inMemoryDatabase() + (("application.secret", "asasasas")))) {
--- test/controllers/ProblemSpec.scala
+++ test/controllers/ProblemSpec.scala
@@ -6,49 +6,64 @@ import play.api.test._
 import play.api.test.Helpers._
 import play.api.i18n.Messages
 import play.api.test.FakeRequest$
-
-
+import org.squeryl.PrimitiveTypeMode.transaction
 
 class ProblemSpec extends Specification with DataTables {
-	"Problem controller" should {
+  "Problem controller" should {
 
     "Display all problems on GET /hackathons/1/problems" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        val result = controllers.Problem.index(1)(FakeRequest())
+        transaction {
+          val dbHackathon = model.Hackathon.lookup(1).get
+          val result = controllers.Problem.index(1)(FakeRequest())
 
-        status(result) must equalTo(OK)
-        contentAsString(result) must contain(helpers.CmsMessages("hackathons.view.title"))
+          status(result) must equalTo(OK)
+
+          val content = contentAsString(result)
+
+          content must contain(dbHackathon.subject)
+          dbHackathon.problems.filter(_.status == model.ProblemStatus.Approved).forall(p => content.contains(p.name)) must beTrue
+        }
+
       }
     }
-    
+
     "Display a problem on GET /hackathons/1/problems/1" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        val result = routeAndCall(FakeRequest(GET, "/hackathons/1/problems/1")).get
+        transaction {
+          val dbHackathon = model.Hackathon.lookup(1).get
+          val result = routeAndCall(FakeRequest(GET, "/hackathons/1/problems/1")).get
 
-        status(result) must equalTo(OK)
-        contentAsString(result) must contain(helpers.CmsMessages("hackathons.view.title"))
+          status(result) must equalTo(OK)
+          
+          val content = contentAsString(result)
+          
+          content must contain(dbHackathon.subject)
+          content must contain(dbHackathon.problems.filter(_.id == 1).head.name)
+        }
       }
     }
-    
+
     "redirect to Login Page if user not logged in" in {
-		""|"httpMethod" | "action"                           |
-		1 ! GET         ! "/hackathons/1/problems/new"       |
-		1 ! POST        ! "/hackathons/1/problems"           |
-		1 ! POST        ! "/hackathons/1/problems/12/update" |
-	    1 ! POST        ! "/hackathons/1/problems/12/delete" |
-	    1 ! GET         ! "/hackathons/1/problems/12/edit"   |
-	    1 ! POST        ! "/hackathons/1/problems/12/update" |
-	    1 ! POST        ! "/hackathons/1/problems/12/delete" |> {
-	    	(justIgnoreMe, httpMethod, action) =>  {
-	    		running(FakeApplication(additionalConfiguration = inMemoryDatabase() + (("application.secret", "asasasas")))) {
-			    val result = routeAndCall(FakeRequest(httpMethod, action)).get
-			
-			    status(result) must equalTo(SEE_OTHER)
-			    redirectLocation(result) must beSome.which(_ == "/login")
-	    	  }
-	    	}
-      }
+      "" | "httpMethod" | "action" |
+        1 ! GET ! "/hackathons/1/problems/new" |
+        1 ! POST ! "/hackathons/1/problems" |
+        1 ! POST ! "/hackathons/1/problems/12/update" |
+        1 ! POST ! "/hackathons/1/problems/12/delete" |
+        1 ! GET ! "/hackathons/1/problems/12/edit" |
+        1 ! POST ! "/hackathons/1/problems/12/update" |
+        1 ! POST ! "/hackathons/1/problems/12/delete" |> {
+          (justIgnoreMe, httpMethod, action) =>
+            {
+              running(FakeApplication(additionalConfiguration = inMemoryDatabase() + (("application.secret", "asasasas")))) {
+                val result = routeAndCall(FakeRequest(httpMethod, action)).get
+
+                status(result) must equalTo(SEE_OTHER)
+                redirectLocation(result) must beSome.which(_ == "/login")
+              }
+            }
+        }
     }
-    
+
   }
 }
