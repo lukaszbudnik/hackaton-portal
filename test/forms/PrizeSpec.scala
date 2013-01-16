dc5a905d93950b025b2f420d24c27542307694dc
bykes <d.bykowski@kainos.com>
2013-01-16 12:37:24 +0100

    fix Prize forms tests

--- test/forms/PrizeSpec.scala
+++ test/forms/PrizeSpec.scala
@@ -1,10 +1,12 @@
 package forms
 
 import java.util.Date
-import java.text.SimpleDateFormat
-import org.specs2.mutable._
-import play.api.test._
-import play.api.test.Helpers._
+
+import org.specs2.mutable.Specification
+
+import controllers.Prize.prizeForm
+import play.api.test.FakeApplication
+import play.api.test.Helpers.running
 
 class PrizeSpec extends Specification {
 
@@ -13,82 +15,91 @@ class PrizeSpec extends Specification {
   "Prize form" should {
 
     "require all fields" in {
+      running(FakeApplication()) {
+        val form = prizeForm.bind(Map.empty[String, String])
 
-      val form = prizeForm.bind(Map.empty[String, String])
+        form.hasErrors must beTrue
+        form.errors.size must equalTo(4)
 
-      form.hasErrors must beTrue
-      form.errors.size must equalTo(4)
+        form("name").hasErrors must beTrue
+        form("description").hasErrors must beTrue
+        form("order").hasErrors must beTrue
+        form("hackathonId").hasErrors must beTrue
 
-      form("name").hasErrors must beTrue
-      form("description").hasErrors must beTrue
-      form("order").hasErrors must beTrue
-      form("hackathonId").hasErrors must beTrue
-
-      form.value must beNone
+        form.value must beNone
+      }
     }
 
     "require name and description and fail if order and hackathonId not filled" in {
-      val form = prizeForm.bind(Map("name" -> "Name", "description" -> "Description"))
+      running(FakeApplication()) {
+        val form = prizeForm.bind(Map("name" -> "Name", "description" -> "Description"))
 
-      form.hasErrors must beTrue
-      form.errors.size must equalTo(2)
+        form.hasErrors must beTrue
+        form.errors.size must equalTo(2)
 
-      // no errors
-      form("name").hasErrors must beFalse
-      form("description").hasErrors must beFalse
+        // no errors
+        form("name").hasErrors must beFalse
+        form("description").hasErrors must beFalse
 
-      // errors
-      form("order").hasErrors must beTrue
-      form("hackathonId").hasErrors must beTrue
+        // errors
+        form("order").hasErrors must beTrue
+        form("hackathonId").hasErrors must beTrue
 
-      form.data must havePair("name" -> "Name")
-      form.data must havePair("description" -> "Description")
+        form.data must havePair("name" -> "Name")
+        form.data must havePair("description" -> "Description")
 
-      form("name").value must beSome.which(_ == "Name")
-      form("description").value must beSome.which(_ == "Description")
+        form("name").value must beSome.which(_ == "Name")
+        form("description").value must beSome.which(_ == "Description")
 
-      form("order").value must beNone
-      form("hackathonId").value must beNone
+        form("order").value must beNone
+        form("hackathonId").value must beNone
 
-      form.value must beNone
+        form.value must beNone
+      }
     }
 
     "validate order and hackathonId as numeric" in {
-      val form = prizeForm.bind(Map("name" -> "Name", "description" -> "Description", "order" -> "_", "hackathonId" -> "_"))
+      running(FakeApplication()) {
+        val form = prizeForm.bind(Map("name" -> "Name", "description" -> "Description", "order" -> "_", "hackathonId" -> "_"))
 
-      form.hasErrors must beTrue
-      form.errors.size must equalTo(2)
+        form.hasErrors must beTrue
+        form.errors.size must equalTo(2)
 
-      form("order").hasErrors must beTrue
-      form("hackathonId").hasErrors must beTrue
+        form("order").hasErrors must beTrue
+        form("hackathonId").hasErrors must beTrue
 
-      form.value must beNone
+        form.value must beNone
+      }
     }
 
     "be filled" in {
-      val form = prizeForm.bind(Map("name" -> "Name", "description" -> "Description", "order" -> "1",
-        "founderName" -> "Founder name", "founderWebPage" -> "Founder web page", "hackathonId" -> "1"))
+      running(FakeApplication()) {
+        val form = prizeForm.bind(Map("name" -> "Name", "description" -> "Description", "order" -> "1",
+          "founderName" -> "Founder name", "founderWebPage" -> "Founder web page", "hackathonId" -> "1"))
 
-      form.hasErrors must beFalse
+        form.hasErrors must beFalse
 
-      form.value must beSome.which {
-        _ match {
-          case (model.Prize("Name", "Description", 1, Some("Founder name"), Some("Founder web page"), 1L)) => true
-          case _ => false
+        form.value must beSome.which {
+          _ match {
+            case (model.Prize("Name", "Description", 1, Some("Founder name"), Some("Founder web page"), 1L)) => true
+            case _ => false
+          }
         }
       }
     }
 
     "be filled from model" in {
-      val date = new Date
-      val form = prizeForm.fill(model.Prize("Name", "Description", 1, Some("Founder name"), Some("Founder web page"), 1L))
+      running(FakeApplication()) {
+        val date = new Date
+        val form = prizeForm.fill(model.Prize("Name", "Description", 1, Some("Founder name"), Some("Founder web page"), 1L))
 
-      form.hasErrors must beFalse
+        form.hasErrors must beFalse
 
-      form.value must beSome.which {
-        _ match {
-          case (model.Prize("Name", "Description", 1, Some("Founder name"), Some("Founder web page"), 1L)) => true
-          case _ => false
+        form.value must beSome.which {
+          _ match {
+            case (model.Prize("Name", "Description", 1, Some("Founder name"), Some("Founder web page"), 1L)) => true
+            case _ => false
+          }
         }
       }
     }
