72feda6f63f437e6ae47b883dddc2855877eac16
Daniel Bykowski <daniel.bykowski@gmail.com>
2013-02-25 08:45:37 +0100

    fix test after update social secure to 2.0.11

--- test/helpers/SecureSocialUtils.scala
+++ test/helpers/SecureSocialUtils.scala
@@ -7,10 +7,7 @@ import play.api.mvc.AnyContent
 import play.api.mvc.Result
 import play.api.test.FakeRequest
 import play.api.test.Helpers.routeAndCall
-import securesocial.core.AuthenticationMethod
-import securesocial.core.SecureSocial
-import securesocial.core.SocialUser
-import securesocial.core.UserId
+import securesocial.core._
 
 /**
  * Util class for secure social framework
@@ -27,12 +24,19 @@ object SecureSocialUtils {
 		transaction {
 			val socialUser = SocialUser(UserId("mockId", "mockProvider"), "mockUser", "mockUser", "mockUser", None, None, AuthenticationMethod.UserPassword)
 			securesocial.core.UserService.save(socialUser)
+		
+			Authenticator.create(socialUser) match {
+				case Right(authenticator) => {
+					val decoratedRequest = request.withCookies(authenticator.toCookie)
+					val result = routeAndCall(decoratedRequest).get
+					result
+				}
+				case Left(error) => {
+					// improve this
+					throw new RuntimeException("Error creating authenticator")
+				}
+			}
 		}
-		
-		val decoratedRequest = request.withSession(SecureSocial.UserKey -> "mockId", SecureSocial.ProviderKey -> "mockProvider", SecureSocial.lastAccess)
-		val result = routeAndCall(decoratedRequest).get
-		result
-		
 	}
 
 	/**
@@ -45,11 +49,18 @@ object SecureSocialUtils {
 		transaction {
 			val socialUser = SocialUser(UserId("mockNormalUserId", "mockProvider"), "mockNormalUser", "mockNormalUser", "mockNormalUser", None, None, AuthenticationMethod.UserPassword)
 			securesocial.core.UserService.save(socialUser)
+
+			Authenticator.create(socialUser) match {
+				case Right(authenticator) => {
+					val decoratedRequest = request.withCookies(authenticator.toCookie)
+					val result = routeAndCall(decoratedRequest).get
+					result
+				}
+				case Left(error) => {
+					// improve this
+					throw new RuntimeException("Error creating authenticator")
+				}
+			}
 		}
-		
-		val decoratedRequest = request.withSession(SecureSocial.UserKey -> "mockNormalUserId", SecureSocial.ProviderKey -> "mockProvider", SecureSocial.lastAccess)
-		val result = routeAndCall(decoratedRequest).get
-		result
-		
 	}
 }
