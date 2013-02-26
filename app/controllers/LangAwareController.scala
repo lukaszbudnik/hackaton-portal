5f1a7aca098c764867d42832369bc9969de010f6
Daniel Bykowski <daniel.bykowski@gmail.com>
2013-02-26 10:34:43 +0100

    on prod use always lang pl

--- app/controllers/LangAwareController.scala
+++ app/controllers/LangAwareController.scala
@@ -1,31 +1,33 @@
 package controllers
 
 import scala.annotation.implicitNotFound
-
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
-
 import org.squeryl.PrimitiveTypeMode.inTransaction
-
 import securesocial.core.SecureSocial
 
 class LangAwareController extends BaseController {
 
-  implicit override def lang(implicit request: RequestHeader): Lang = {
-    val session = request.session
+	implicit override def lang(implicit request: RequestHeader): Lang = {
 
-    val sessionLanguage = session.get(LangAwareController.SESSION_LANG_KEY)
-    
-    sessionLanguage.map { langString =>
-      Lang(langString)
-    }.getOrElse(super.lang)
-    
-  }
+		if (play.Play.isProd) {  
+			Lang("pl")
+		} else {
+		
+			val session = request.session
+			val sessionLanguage = session.get(LangAwareController.SESSION_LANG_KEY)
+
+			sessionLanguage.map { langString =>
+				Lang(langString)
+			}.getOrElse(super.lang)
+		
+		}
+	}
 
 }
 
 object LangAwareController {
-  val SESSION_LANG_KEY = "lang"
-  val SESSION_LANG_CHANGED_AFTER_LOGIN = "lang-changed-after-login"
-  val DEFAULT_LANG = "en"
+	val SESSION_LANG_KEY = "lang"
+	val SESSION_LANG_CHANGED_AFTER_LOGIN = "lang-changed-after-login"
+	val DEFAULT_LANG = "en"
 }
